4db4440fdcab89d1de99eb1fddca7004a3cd674b
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2020-01-23 18:16:50 -0700

    fixed memory leak

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -11,7 +11,7 @@ export default function Friends () {
         .get('/api/friends', localStorage.getItem('token'))
         .then(res => setFriends(res.data))
         .catch(err => console.log(err))
-    })
+    },[])
     return (
         <>
         <Link to='/add'>Add Friend</Link>
